d4bd407c818a9faac3151a3152c92c05f712ac54
chinesecold <chinesecold@gmail.com>
2013-01-26 22:00:11 -0500

    Twitter Trends added

---
+++ src/testApp.cpp
@@ -0,0 +1,177 @@
+// A side project receiving face tracking osc messages from
+// Kyle McDonald's FaceOSC https://github.com/kylemcdonald/ofxFaceTracker/downloads
+// for the IACD class at the CMU School of Art
+//
+//
+// Author: Han Hua, Jan 2013
+//
+// Built on a template from Dan Wilcox danomatika.com
+// for the IACD class at the CMU School of Art
+//
+// adapted from Greg Borenstein's Processing example
+// https://gist.github.com/1603230
+//
+// Also referenced to ofxJSON https://github.com/jefftimesten/ofxJSON
+//
+
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup() {
+    // set bg to a nice grey!
+	ofBackground(150);
+    
+    // load fonts to display stuff
+	font.loadFont("futura_book.otf", 12);
+	titleFont.loadFont("futura_book.otf", 20);
+    
+    // Enable some logging information
+	ofSetLogLevel(OF_LOG_VERBOSE);
+    
+    //franklinBook.loadFont("frabk.ttf", 32);
+	franklinBook.setLineHeight(36);
+    
+    string url = "http://api.twitter.com/1/trends/1.json";
+    //string url = "http://api.twitter.com/1/trends/daily.json";
+    //string url = "http://api.twitter.com/1.1/trends/closest.json";
+    //string url = "http://api.twitter.com/1.1/search/tweets.json?q=Google";
+    //string url = "http://search.twitter.com/search.json?q=%40AlanHuaCN&result_type=mixed";
+
+    // FaceOSC sends to port 8338 by default
+	receiver.setup(8338);
+
+	ofBackground(255);
+    
+    // Now parse the JSON
+	parsed = json.open(url);
+/*	if ( parsed )
+    {
+		cout << json.getRawString() << endl;
+		
+	}
+    else
+    {
+		cout  << "Failed to parse JSON" << endl;
+	} */
+
+}
+
+//--------------------------------------------------------------
+void testApp::update() {
+
+	// check for waiting osc messages
+	while(receiver.hasWaitingMessages())
+	{
+		// get the next message
+		ofxOscMessage m;
+		receiver.getNextMessage(&m);
+        
+        // load face from any FaceOSC messages
+        face.parseOSC(m);
+	}
+    
+    // found face?
+    if(face.found > 0) {
+    //cout << "---------" << endl
+      //   << face.toString();
+    }
+}
+
+
+//--------------------------------------------------------------
+void testApp::draw() {
+
+    
+    if(face.found > 0) {
+        // draw a face
+        
+        ofPushMatrix();
+            ofTranslate(face.posePosition);
+            ofScale(face.poseScale, face.poseScale);
+            
+            ofSetColor(0);
+            ofNoFill();
+            ofEllipse(-20, face.eyeLeft * -9, 20, 7);
+            ofEllipse(20, face.eyeRight * -9, 20, 7);
+            ofEllipse(0, 20, face.mouthWidth * 3, face.mouthHeight * 3);
+            ofEllipse(-5, face.nostrils * -1, 7, 3);
+            ofEllipse(5, face.nostrils * -1, 7, 3);
+            
+            ofSetRectMode(OF_RECTMODE_CENTER);
+            ofFill();
+            ofRect(-20, face.eyebrowLeft * -5, 25, 5);
+            ofRect(20, face.eyebrowRight * -5, 25, 5);
+            ofSetRectMode(OF_RECTMODE_CORNER);
+        ofPopMatrix();
+        
+        
+        // catch emotion
+        if(face.mouthHeight > 5){
+            //printf("Open your mouth!");
+            if(parsed)
+            {
+                ofSetHexColor(0x000000);
+                
+                int n=0;
+                ofxJSONElement trends = json[n]["trends"];
+                
+                for(int i=0; i<trends.size(); i++)
+                {
+                    string message = trends[i]["query"].asString();
+                    message = ofxJSONElement::decodeURL( message );
+                    cout << message << endl;
+                    font.drawString(message, 10, 40*i+40);
+                    //franklinBook.drawString(message, 10, 40*i+40);
+                    
+                }
+            }
+             
+             
+        }
+    }
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed (int key) {
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ) {
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button) {
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo) { 
+
+}
---
+++ src/testApp.h
@@ -0,0 +1,46 @@
+//
+// a template for receiving face tracking osc messages from
+// Kyle McDonald's FaceOSC https://github.com/kylemcdonald/ofxFaceTracker/downloads
+//
+// 2012 Dan Wilcox danomatika.com
+// for the IACD class at the CMU School of Art
+//
+// adapted from Greg Borenstein's Processing example
+// https://gist.github.com/1603230
+//
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOsc.h"
+#include "ofxJSONElement.h"
+
+#include "Face.h"
+
+class testApp : public ofBaseApp {
+	
+    public:
+
+		void setup();
+		void update();
+		void draw();
+		
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		ofxOscReceiver	receiver;
+        Face face;
+        ofTrueTypeFont franklinBook;
+        ofxJSONElement json;
+        bool parsed;
+    
+        ofTrueTypeFont font; // standard font
+        ofTrueTypeFont titleFont; // font for some info in the title line
+
+};
